c3f0f62140dc1a12040df21eba07e295e96e6a9a
Yasin Uslu <nepjua@gmail.com>
2015-02-19 19:11:05 +0200

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'yasinuslu:oneplace',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'Keep everything in one place',
   // URL to the Git repository containing the source code for this package.
